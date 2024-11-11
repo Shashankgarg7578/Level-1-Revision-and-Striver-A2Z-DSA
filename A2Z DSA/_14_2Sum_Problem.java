9a028ea3c05a1e27c30eacbdc48fa8e5c38c0755
Shashank Garg <47049430+Shashankgarg7578@users.noreply.github.com>
2024-11-11 17:51:39 +0530

    Solved Arrays on 11Nov2024 and solve 4 problems
    Solved Arrays on 11Nov2024 and solve 4 problems
    Solve problems on 3rd Day
    1.) 2Sum Problem
    2.) Sort an array of 0's 1's and 2's
    3.) Majority Element (>n/2 times)
    4.) Kadane's Algorithm, maximum subarray sum

---
+++ A2Z DSA/_14_2Sum_Problem.java
@@ -0,0 +1,99 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+//blog: https://takeuforward.org/data-structure/two-sum-check-if-a-pair-with-given-sum-exists-in-array/
+//Question : https://leetcode.com/problems/two-sum/
+public class _14_2Sum_Problem {
+	public static void main(String[] args) {
+		int n = 5;
+		int[] arr = { 2, 6, 5, 8, 11 };
+		int target = 14;
+		int[] ans = twoSum(n, arr, target);
+		System.out.println("This is the answer for variant 1: " + ans[0] + " " + ans[1]);
+
+		int[] ans2 = twoSum2(n, arr, target);
+		System.out.println("This is the answer for variant 1: " + ans2[0] + " " + ans2[1]);
+
+		int n3 = 3;
+		int[] arr3 = { 3, 2, 4 };
+		int target3 = 6;
+		String ans3 = twoSum3(n3, arr3, target3);
+		System.out.println("This is the answer for variant 1: " + ans3);
+
+	}
+
+	// Brute Force
+	// Time Complexity: O(N2)
+	// Space Complexity: O(1)
+	private static int[] twoSum(int n, int[] arr, int target) {
+
+		int[] ans = new int[2];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+
+				if (i == j) {
+					continue;
+				} else {
+					if (arr[i] + arr[j] == target) {
+						ans[0] = i;
+						ans[1] = j;
+						return ans;
+					}
+				}
+
+			}
+		}
+
+		return ans;
+	}
+
+	// Better
+	// Time Complexity: O(N)
+	// Space Complexity: O(N)
+	private static int[] twoSum2(int n, int[] arr, int target) {
+
+		int[] ans = new int[2];
+
+		// key = value of array, value = index of array
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+		for (int i = 0; i < n; i++) {
+			if (map.containsKey(target - arr[i])) {
+				ans[0] = map.get(target - arr[i]);
+				ans[1] = i;
+			} else {
+				map.put(arr[i], i);
+			}
+		}
+
+		return ans;
+	}
+
+	// Optimal : Two Pointer , but it is valid only for no or yes, 
+	//           if we want to return index then it will not solve by Optimal approach because we need to store somewhere.
+	// Time Complexity:
+	// Space Complexity:
+	private static String twoSum3(int n, int[] arr, int target) {
+		Arrays.sort(arr);
+		
+		int left = 0;
+		int right = arr.length - 1;
+
+		while (left <= right) {
+			if (arr[left] + arr[right] < target) {
+				left++;
+			} else if (arr[left] + arr[right] > target) {
+				right--;
+			} else if (arr[left] + arr[right] == target) {
+				return "YES";
+			}
+
+		}
+
+		return "NO";
+	}
+
+}
---
+++ A2Z DSA/_17_Kadanes_Algorithm_maximum_subarray_sum.java
@@ -0,0 +1,101 @@
+package Arrays;
+
+//blog: https://takeuforward.org/data-structure/kadanes-algorithm-maximum-subarray-sum-in-an-array/
+//Question : https://leetcode.com/problems/maximum-subarray/
+public class _17_Kadanes_Algorithm_maximum_subarray_sum {
+
+	public static void main(String[] args) {
+		int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+		int n = arr.length;
+		int maxSum = maxSubarraySum(arr, n);
+		System.out.println("The maximum subarray sum is: " + maxSum);
+
+		int maxSum2 = maxSubarraySum2(arr, n);
+		System.out.println("The maximum subarray sum is: " + maxSum2);
+
+		int maxSum3 = maxSubarraySum3(arr, n);
+		System.out.println("The maximum subarray sum is: " + maxSum3);
+	}
+
+	// Brute force
+	// Time Complexity: O(N3)
+	// Space Complexity: O(1)
+	private static int maxSubarraySum(int[] arr, int n) {
+		int maxSum = Integer.MIN_VALUE;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j < arr.length; j++) {
+
+				int sum = 0;
+				for (int k = i; k <= j; k++) {
+					sum += arr[k];
+				}
+				if (maxSum < sum) {
+					maxSum = sum;
+				}
+			}
+		}
+
+		return maxSum;
+	}
+
+	// Better
+	// Time Complexity: O(N^2)
+	// Space Complexity: O(1)
+	private static int maxSubarraySum2(int[] arr, int n) {
+
+		int maxSum = Integer.MIN_VALUE;
+		for (int i = 0; i < arr.length; i++) {
+			int sum = 0;
+			for (int j = i; j < arr.length; j++) {
+				sum += arr[j];
+				if (maxSum < sum) {
+					maxSum = sum;
+				}
+			}
+		}
+
+		return maxSum;
+	}
+
+	// Optimal : Kadane's Algorithm
+	// Time Complexity: O(N)
+	// Space Complexity: O(1)
+	private static int maxSubarraySum3(int[] arr, int n) {
+
+		int maxSum = Integer.MIN_VALUE;
+
+		int sum = 0;
+		for(int i = 0; i < arr.length ; i++) {
+			sum += arr[i];
+			
+			if(sum > maxSum) {
+				maxSum = sum;
+			}
+			
+			// If sum < 0: discard the sum calculated
+			if(sum < 0) {
+				sum = 0;
+			}
+		}
+
+        // To consider the sum of the empty subarray
+        // uncomment the following check:
+
+        //if (maxi < 0) maxi = 0;
+
+		return maxSum;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
